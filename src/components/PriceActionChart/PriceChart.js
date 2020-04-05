368eec7bdb9ed8cce2ad6d696aabd0de0885fcaf
Pritam <pritam.sarkar@medlife.com>
2020-04-05 23:34:03 +0530

    Add average path

--- src/components/PriceActionChart/PriceChart.js
+++ src/components/PriceActionChart/PriceChart.js
@@ -1,6 +1,6 @@
 import * as d3 from "d3";
-import {scaleLinear} from "d3";
-import {select} from "d3-selection";
+import {scaleLinear, scaleBand} from "d3";
+import {select, selectAll} from "d3-selection";
 import moment from "moment";
 import React from 'react';
 import {generatePriceGraphDataForThisDay, getInitialData} from "../../services/faker/PriceGraphDataGenerator";
@@ -42,12 +42,28 @@ class PriceChart extends React.Component {
         const candleOCGap = 6;
         const candleHLWidth = 2;
         const animationDuration = 500;
+        const yScale = scaleLinear().domain([priceMin, priceMax]).range([0, svgDimensions.height]);
 
-        const yScale = scaleLinear().domain([priceMin, priceMax]).range([0, svgDimensions.width]);
+        select("svg").selectAll("g.x-axis").remove();
+        const domain = Object.keys(data).map((v, i) => (i%5 === 0 ? v : ""));
+        const x_axis_scale = scaleBand()
+            .range([0, (candleOCWidth + candleOCGap) * (Object.keys(data).length - 1)])
+            .domain(domain);
+        const x_axis = d3.axisTop().scale(x_axis_scale);
+        select("svg").append("g")
+            .attr('class', 'axis x-axis')
+            .attr("transform", `translate(0, ${svgDimensions.width - 5})`)
+            .call(x_axis);
 
-        const y_axis_scale = scaleLinear().domain([priceMax, priceMin]).range([0, svgDimensions.width]);
+        select("svg").selectAll("g.y-axis").remove();
+        const y_axis_scale = scaleLinear()
+            .domain([priceMax, priceMin])
+            .range([0, svgDimensions.height]);
         const y_axis = d3.axisLeft().scale(y_axis_scale);
-        select("svg").append("g").attr('class', 'axis y-axis').attr("transform", "translate(495, 0)").call(y_axis);
+        select("svg").append("g")
+            .attr('class', 'axis y-axis')
+            .attr("transform", `translate(${svgDimensions.height - 5}, 0)`)
+            .call(y_axis);
 
         const updateOCBlockColorAndDimensions = function(d) {
             d3.select(this)
@@ -99,7 +115,19 @@ class PriceChart extends React.Component {
                 return (data[d].close - data[d].open);
             });
 
-        select(node).selectAll('g').exit()
+        select(node).selectAll('g').exit();
+
+        let lineGenerator = d3.line().curve(d3.curveCatmullRom);
+        let avg_data = Object.keys(data).map((d, i) => {
+            return [(candleOCWidth + candleOCGap)*i + candleOCWidth/2, (svgDimensions.width - yScale(data[d].avg))]
+        });
+        let pathString = lineGenerator(avg_data);
+        select("svg").selectAll("path.avg-price-line").remove();
+        select("svg").append("path")
+            .style("stroke", "steelblue")
+            .style("fill", "none")
+            .attr("class", "line avg-price-line")
+            .attr("d", pathString);
     }
 
     addNextData() {
